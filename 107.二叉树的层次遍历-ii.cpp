d3e58b5b93f651e0ed59d06741d0a3f74700a0ea
liangshengnan <liangshengnan@inspur.com>
2020-09-24 19:29:25 +0800

    宽度优先遍历

---
+++ 107.二叉树的层次遍历-ii.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=cpp
+ *
+ * [107] 二叉树的层次遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        auto levelOrder = vector<vector<int>>();
+        if (!root) {
+            return levelOrder;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            auto level = vector<int>();
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            levelOrder.push_back(level);
+        }
+        reverse(levelOrder.begin(), levelOrder.end());
+        return levelOrder;
+    }
+};
+// @lc code=end
+
